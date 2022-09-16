d75e2bc127c17191a9baaa5bc5dab5865ffc04bd
Winston Limanto <wmlimanto@gmail.com>
2022-09-16 14:21:39 -0700

    add controller for user sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,16 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
   private
 
   def user_params
