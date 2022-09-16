5e96e75b3147bcee79fd945db076641a6a2682a8
Winston Limanto <wmlimanto@gmail.com>
2022-09-15 19:23:28 -0700

    add db migration and schema

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,15 @@ ActiveRecord::Schema.define(version: 2021_06_26_062916) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  create_table "users", force: :cascade do [t]
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
