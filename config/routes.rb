875690d4f989a62da958adf0a29fbba9d55666b7
Winston Limanto <wmlimanto@gmail.com>
2022-09-15 17:21:26 -0700

    add about us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/help'
+  get 'static_pages/faq'
+  get 'static_pages/about'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  resource :about, only: [:show]
 
   resource :cart, only: [:show] do
     post   :add_item
