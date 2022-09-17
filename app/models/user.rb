98f7ff63a67070f87c519373d22807025527e059
Winston Limanto <wmlimanto@gmail.com>
2022-09-17 15:22:01 -0700

    add test to authenticate user with credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+  validates :password, length: { minimum: 8 }, presence: true
+  validates :password_confirmation, presence: true
+
+  def authenticate_with_credentials(email, password)
+    if self.authenticate(password)
+      return self
+    else
+      return nil
+    end
+  end
+  
 end
